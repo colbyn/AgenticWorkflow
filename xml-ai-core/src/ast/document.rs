e9605ef2c5e847fcf2efa33551ae3c73e7fc24c1
Colbyn Wadman <hello@colbyn.com>
2025-07-22 14:50:41 -0600

    update document API

--- xml-ai-core/src/ast/document.rs
+++ xml-ai-core/src/ast/document.rs
@@ -17,3 +17,13 @@ impl DocumentChildNode {
 pub struct DocumentNode {
     pub children: Vec<DocumentChildNode>,
 }
+
+impl DocumentNode {
+    /// TODO: proper error handling.
+    pub fn from_str(source: &str) -> Result<Self, Box<dyn std::error::Error>> {
+        let html_tree = html_ast::parser::parse_from_fragment(source);
+        let html_tree = html_tree.expect("valid html document");
+        let document = DocumentNode::from_node(html_tree).expect("should be a valid node");
+        Ok(document)
+    }
+}
